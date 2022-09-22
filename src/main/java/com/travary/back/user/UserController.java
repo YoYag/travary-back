89339b3b7e8ee926c0ddb623ad7db31590b82c7c
YoYag <la2043432@gmail.com>
2022-09-22 11:40:31 +0900

    회원가입 구현

--- src/main/java/com/travary/back/user/UserController.java
+++ src/main/java/com/travary/back/user/UserController.java
@@ -1,7 +1,6 @@
 package com.travary.back.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,20 +9,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private UserService userService;
 
     @PostMapping("/join")
     @ResponseBody
     public String join(User user) {
-        System.out.println(user);
-        user.setRole("ROLE_USER");
-        String rawPassword = user.getPassword();
-        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
-        user.setPassword(encPassword);
-        userRepository.save(user);
         return "회원가입완료";
     }
 }
---
+++ src/main/java/com/travary/back/user/UserService.java
@@ -0,0 +1,27 @@
+package com.travary.back.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void join(User user) {
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+    }
+
+}
